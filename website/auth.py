937ff52f08cec999722bfc4afdfb761f8063de5a
Joseph-gift <godonoujoseph.jg@gmail.com>
2025-04-20 17:45:49 +0000

    design sign up and login page, handling post request

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,34 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 # Defining a blueprint for the website module
 auth = Blueprint("auth", __name__)
 
-@auth.route("/login")
+@auth.route("/login", methods=["GET","POST"])
 def login():
+    data = request.form
     return render_template("login.html",boolean=True)
 
 @auth.route("/logout")
 def logout():
     return render_template("home.html")
 
-@auth.route("/signup")
+@auth.route("/signup",  methods=["GET","POST"])
 def signup():
+    if request.method == "POST":
+        email = request.form.get('email')
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmPassword = request.form.get("confirmPassword")
+
+        if len(email) < 4:
+            pass
+        elif len(username) < 2:
+            pass
+        elif password != confirmPassword:
+            pass
+        elif len(password) < 7:
+            pass
+        else:
+            # Add User to database
+            pass
     return render_template("signup.html")
