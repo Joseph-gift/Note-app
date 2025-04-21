a4da2d8a859c6b3f5a012acba74ee522993b0cc9
Joseph-gift <godonoujoseph.jg@gmail.com>
2025-04-21 20:51:06 +0000

    add message flashing

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,26 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# Initializing the SQLAlchemy object
+# This object will be used to interact with the database
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 # Creating a flask application instance
 # This is the entry point for the Flask application
 def create_app():
+    '''
+     Creating a Flask application instance
+     The __name__ argument is used to determine the root path of the application
+    '''
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'mysecret_key'
+    # Setting the configuration for the SQLAlchemy database URI
+    # The database URI specifies the type of database and its location
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    
+    # Initializing the database with the application instance
+    db.init_app(app)
 
     # Registering the blueprints for the website module
     from .views import views
--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 # Defining a blueprint for the website module
 auth = Blueprint("auth", __name__)
@@ -21,14 +21,14 @@ def signup():
         confirmPassword = request.form.get("confirmPassword")
 
         if len(email) < 4:
-            pass
-        elif len(username) < 2:
-            pass
-        elif password != confirmPassword:
-            pass
+            flash("Your Email is not valid", category="error")
+        elif len(username) < 3:
+            flash("Your username must be greater than 2 character", category="error")
         elif len(password) < 7:
-            pass
+            flash("Your password is too short", category="error")
+        elif password != confirmPassword:
+            flash("Your password do not match", category="error")
         else:
             # Add User to database
-            pass
+            flash("Account created succefully", category="succes")
     return render_template("signup.html")
